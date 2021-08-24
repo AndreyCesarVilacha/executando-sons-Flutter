0b84c42854bacc85b506746ba299e6b8e5d5945a
Andrey Cesar Vilachã <andreycesarrodrigues@hotmail.com>
2021-08-24 07:48:03 -0300

    Apontando para uma versão mais nova do kotlin

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+//audioplayers: ^0.19.1
+import 'package:audioplayers/audioplayers.dart';
 
 class Home extends StatefulWidget {
   const Home({ Key? key }) : super(key: key);
@@ -8,8 +10,20 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  AudioPlayer audioPlayer = AudioPlayer();
+
+  _executar() async{
+
+    String url = "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-3.mp3";
+
+    int resultado = await audioPlayer.play(url);
+
+  }
+
   @override
   Widget build(BuildContext context) {
+    _executar();
     return Container(
       
     );
